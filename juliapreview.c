08700d866e3954fe9d06584239746f130e0328af
orborde <orborde@d4908a15-9b18-0410-9c1c-f40699104922>
2008-12-25 03:21:21 +0000

    - Do some cleanup of juliapreview - Also, it's doing z^6 + c...
    git-svn-id: file:///home/orborde/svn-main/devel/fractal/trunk/tools@489 d4908a15-9b18-0410-9c1c-f40699104922

--- juliapreview.c
+++ juliapreview.c
@@ -5,7 +5,7 @@
   USAGE:
   Can be called with no arguments
   juliapreview LEFTX TOPY RIGHTX BOTTOMY will set the Mandelbrot's region
-    to the given coordinates
+  to the given coordinates
 */
 
 #include <SDL/SDL.h>
@@ -57,18 +57,20 @@ Uint32 visualize_rgb(Uint32 iters, Uint32 period, Uint32 maxiters);
 
 /* Draws the mandelbrot onscreen using the given colormap */
 void draw_mandelbrot(SDL_Surface * screen,
-		     complex_region region, SDL_Rect screen_region,
-		     Uint32 * colormap,
-		     unsigned maxiters);
+                     complex_region region, SDL_Rect screen_region,
+                     Uint32 * colormap,
+                     unsigned maxiters);
 unsigned mandelbrot_iterate(complex c, unsigned maxiters);
 /* Draws the Julia onscreen using the given colormap */
 void draw_julia(SDL_Surface * screen,
-		complex_region region, SDL_Rect screen_region,
-		Uint32 * colormap,
-		unsigned maxiters,
-		complex c);
+                complex_region region, SDL_Rect screen_region,
+                Uint32 * colormap,
+                unsigned maxiters,
+                complex c);
 unsigned julia_iterate(complex z, const complex c, double escape,
-		       unsigned maxiters);
+                       unsigned maxiters);
+
+double linmap(double x1, double y1, double x2, double y2, double xt);
 
 /* Main Function */
 int main (int argc, char * argv[])
@@ -85,22 +87,22 @@ int main (int argc, char * argv[])
       mandelbrot_region = temp;
 
       fprintf(stderr,"Mandelbrot region set to (%lf,%lf) - (%lf,%lf)\n",
-	      leftx, topy, rightx, bottomy);
+              leftx, topy, rightx, bottomy);
     }
 
   /* SDL initialization stuff */
   // init video, check for success
   if (SDL_Init(SDL_INIT_VIDEO) < 0)
     { fprintf(stderr,"Video init failed\n"); return -1; };
-  
+
   // reg SDL_Quit to be called at exit to clean up
   atexit(SDL_Quit);
-  
+
   fprintf(stderr,"SDL is up and running!\n");
-  
+
   if (configure_video(DEFAULT_WIDTH, DEFAULT_HEIGHT))
     { fprintf(stderr,"Video modeset failed\n"); return -1; };
-  fprintf(stderr,"Video is set!\n");  
+  fprintf(stderr,"Video is set!\n");
 
   /*** Our initialization stuff ***/
   fprintf(stderr,"Now setting up fractal things\n");
@@ -110,8 +112,8 @@ int main (int argc, char * argv[])
     unsigned i;
     for (i=0; i<=MAXITERS; i++)
       {
-	colormap[i] =
-	  visualize_rgb(i, RGB_PERIOD, MAXITERS);
+        colormap[i] =
+          visualize_rgb(i, RGB_PERIOD, MAXITERS);
       }
   }
   // Draw the mandelbrot
@@ -119,7 +121,7 @@ int main (int argc, char * argv[])
     fprintf(stderr,"Rendering mandelbrot...\n");
     Uint32 start = SDL_GetTicks();
     draw_mandelbrot(screen, mandelbrot_region, mandelbrot_screen,
-		    colormap, MAXITERS);
+                    colormap, MAXITERS);
     Uint32 stop = SDL_GetTicks();
     fprintf(stderr,"  Mandelbrot took %lums\n", stop - start);
   }
@@ -138,76 +140,88 @@ int main (int argc, char * argv[])
       // handle events
       SDL_Event event;
       while(SDL_PollEvent(&event))
-	{
-	  switch(event.type)
-	    {
-	    case SDL_VIDEORESIZE:
-	      // Reconfigure video
-	      if (configure_video(event.resize.w, event.resize.h))
-		{
-		  fprintf(stderr,"Error on video reconfigure! Quitting...\n");
-		  return -1;
-		}
-	      // Redraw the Mandelbrot and current Julia in their new regions
-	      draw_mandelbrot(screen, mandelbrot_region, mandelbrot_screen,
-			      colormap, MAXITERS);
-	      draw_julia(screen, julia_region, julia_screen,
-			 colormap, MAXITERS, c);
-	      break;
-	    case SDL_KEYDOWN:
-	      switch(event.key.keysym.sym)
-		{
-		case SDLK_ESCAPE:
-		  return 0;
-		  break;
-		}
-	      break;
-	    case SDL_QUIT:
-	      return 0;
-	      break;
-	    }
-	} // poll
+        {
+          switch(event.type)
+            {
+            case SDL_VIDEORESIZE:
+              // Reconfigure video
+              if (configure_video(event.resize.w, event.resize.h))
+                {
+                  fprintf(stderr,"Error on video reconfigure! Quitting...\n");
+                  return -1;
+                }
+              // Redraw the Mandelbrot and current Julia in their new regions
+              draw_mandelbrot(screen, mandelbrot_region, mandelbrot_screen,
+                              colormap, MAXITERS);
+              draw_julia(screen, julia_region, julia_screen,
+                         colormap, MAXITERS, c);
+              break;
+            case SDL_KEYDOWN:
+              switch(event.key.keysym.sym)
+                {
+                case SDLK_ESCAPE:
+                  return 0;
+                  break;
+                }
+              break;
+            case SDL_QUIT:
+              return 0;
+              break;
+            }
+        } // poll
       // Read mouse state and rerender Julia if needed
       {
-	int x,y;
-	if (SDL_GetMouseState(&x, &y) & SDL_BUTTON(1))
-	  {
-	    if (x >= mandelbrot_screen.x &&
-		y >= mandelbrot_screen.y &&
-		x < mandelbrot_screen.x + mandelbrot_screen.w &&
-		y < mandelbrot_screen.y + mandelbrot_screen.h)
-	      {
-		c.r =
-		  mandelbrot_region.topleft.r + 
-		  (x - mandelbrot_screen.x) *
-		  (mandelbrot_region.bottomright.r
-		   - mandelbrot_region.topleft.r) /
-		  mandelbrot_screen.w;
-		c.i =
-		  mandelbrot_region.topleft.i + 
-		  (y - mandelbrot_screen.y) *
-		  (mandelbrot_region.bottomright.i
-		   - mandelbrot_region.topleft.i) /
-		  mandelbrot_screen.h;
-		draw_julia(screen, julia_region, julia_screen,
-			   colormap, MAXITERS, c);
-	      } // In the mandelbrot region?
-	  } // Pressed left mouse button?
+        int x,y;
+        static int zoom_selecting = 0;
+        static complex zoom_upperleft = {0, 0};
+        Uint8 state = SDL_GetMouseState(&x, &y);
+        if (x >= mandelbrot_screen.x &&
+            y >= mandelbrot_screen.y &&
+            x < mandelbrot_screen.x + mandelbrot_screen.w &&
+            y < mandelbrot_screen.y + mandelbrot_screen.h)
+          {
+            // If left mouse button pressed, redraw a Julia
+            if (state & SDL_BUTTON(1))
+              {
+                c.r = linmap(mandelbrot_screen.x,
+                             mandelbrot_region.topleft.r,
+                             mandelbrot_screen.x+mandelbrot_screen.w,
+                             mandelbrot_region.bottomright.r,
+                             x);
+                c.i = linmap(mandelbrot_screen.y,
+                             mandelbrot_region.topleft.i,
+                             mandelbrot_screen.y+mandelbrot_screen.h,
+                             mandelbrot_region.bottomright.i,
+                             y);
+                draw_julia(screen, julia_region, julia_screen,
+                           colormap, MAXITERS, c);
+              } // Left mouse button action
+
+          } // In the mandelbrot region?
+        else
+          {
+            zoom_selecting = 0;
+          }
       } // Mouse state reading block
     } // main loop
   return 0;
 }
 
+double linmap(double x1, double y1, double x2, double y2, double xt)
+{
+  return y1 + (y2 - y1)*(xt - x1)/(x2 - x1);
+}
+
 int configure_video(int width, int height)
 {
   /*  // Reset global screen dimensions according to what changed
-  if (width != WIDTH)
-    {
+      if (width != WIDTH)
+      {
       HEIGHT = width/2;
       WIDTH = HEIGHT * 2;
-    }
-  else if (height != HEIGHT)
-    {
+      }
+      else if (height != HEIGHT)
+      {
       HEIGHT = height;
       WIDTH = HEIGHT * 2;
       }*/
@@ -247,7 +261,7 @@ Uint32 visualize_rgb(Uint32 iters, Uint32 period, Uint32 maxiters)
     return SDL_MapRGB(screen->format, 0, 0, 0);
 
   iters %= (period * 3);
-  Uint32 value = 
+  Uint32 value =
     (iters % period) * 255 / period;
   if (iters >= (period * 2))
     return SDL_MapRGB(screen->format, value, 0, 0);
@@ -258,32 +272,32 @@ Uint32 visualize_rgb(Uint32 iters, Uint32 period, Uint32 maxiters)
 }
 
 void draw_mandelbrot(SDL_Surface * screen,
-		     complex_region region, SDL_Rect screen_region,
-		     Uint32 * colormap,
-		     unsigned maxiters)
+                     complex_region region, SDL_Rect screen_region,
+                     Uint32 * colormap,
+                     unsigned maxiters)
 {
   // lock teh surface
   if (SDL_MUSTLOCK(screen))
     if (SDL_LockSurface(screen) < 0) return;
-    
+
   int i,j;
   for (j=0; j<screen_region.h; j++)
     {
       complex c = {0,0};
       c.i = region.topleft.i +
-	(region.bottomright.i - region.topleft.i) * j / screen_region.h;
+        (region.bottomright.i - region.topleft.i) * j / screen_region.h;
 
       for (i=0; i<screen_region.w; i++)
-	{
-	  c.r =
-	    region.topleft.r +
-	    (region.bottomright.r - region.topleft.r) * i / screen_region.w;
-	  unsigned iters = mandelbrot_iterate(c, maxiters);
-	  //	  fprintf(stderr,"%lf,%lf=%u(%lu) ", c.r, c.i, iters,colormap[iters]);
-	  Uint32 color =
-	    colormap[iters];
-	  putPixel(screen, screen_region.x + i, screen_region.y + j, color);
-	}
+        {
+          c.r =
+            region.topleft.r +
+            (region.bottomright.r - region.topleft.r) * i / screen_region.w;
+          unsigned iters = mandelbrot_iterate(c, maxiters);
+          //      fprintf(stderr,"%lf,%lf=%u(%lu) ", c.r, c.i, iters,colormap[iters]);
+          Uint32 color =
+            colormap[iters];
+          putPixel(screen, screen_region.x + i, screen_region.y + j, color);
+        }
       //      putchar('\n');
     }
 
@@ -292,55 +306,61 @@ void draw_mandelbrot(SDL_Surface * screen,
     SDL_UnlockSurface(screen);
 
   // update!
-  /*SDL_UpdateRect(screen, 
-		 screen_region.x, screen_region.y,
-		 screen_region.w, screen_region.h);*/
+  /*SDL_UpdateRect(screen,
+    screen_region.x, screen_region.y,
+    screen_region.w, screen_region.h);*/
   //  SDL_UpdateRect(screen, 0,0,0,0);
-    SDL_UpdateRects(screen, 1, &screen_region);
+  SDL_UpdateRects(screen, 1, &screen_region);
 
 
+}
+
+unsigned iterate(complex z, complex c, unsigned maxiters)
+{
+  unsigned iters = 0;
+
+  while (++iters < maxiters && complex_sqmag(z) <= 4)
+    z = complex_add(complex_mult(z,complex_mult(z,complex_mult(z,complex_mult(z,complex_mult(z, z))))), c);
+
+  return iters;
 }
 
 unsigned mandelbrot_iterate(complex c, unsigned maxiters)
 {
   complex z = {0,0};
-  unsigned iters = 0;
-  while (++iters < maxiters && complex_sqmag(z) <= 4)
-    z = complex_add(complex_mult(z, z), c);
-
-  return iters;
+  return iterate(z,c, maxiters);
 }
 
 
 void draw_julia(SDL_Surface * screen,
-		complex_region region, SDL_Rect screen_region,
-		Uint32 * colormap,
-		unsigned maxiters,
-		complex c)
+                complex_region region, SDL_Rect screen_region,
+                Uint32 * colormap,
+                unsigned maxiters,
+                complex c)
 {
   printf("c=(%lf,%lf)\n", c.r, c.i);
   // lock teh surface
   if (SDL_MUSTLOCK(screen))
     if (SDL_LockSurface(screen) < 0) return;
-    
+
   int i,j;
   for (j=0; j<screen_region.h; j++)
     {
       complex z = {0,0};
       z.i = region.topleft.i +
-	(region.bottomright.i - region.topleft.i) * j / screen_region.h;
+        (region.bottomright.i - region.topleft.i) * j / screen_region.h;
 
       for (i=0; i<screen_region.w; i++)
-	{
-	  z.r =
-	    region.topleft.r +
-	    (region.bottomright.r - region.topleft.r) * i / screen_region.w;
-	  unsigned iters = julia_iterate(z,c,2, maxiters);
-	  //	  fprintf(stderr,"%lf,%lf=%u(%lu) ", c.r, c.i, iters,colormap[iters]);
-	  Uint32 color =
-	    colormap[iters];
-	  putPixel(screen, screen_region.x + i, screen_region.y + j, color);
-	}
+        {
+          z.r =
+            region.topleft.r +
+            (region.bottomright.r - region.topleft.r) * i / screen_region.w;
+          unsigned iters = julia_iterate(z,c,2, maxiters);
+          //      fprintf(stderr,"%lf,%lf=%u(%lu) ", c.r, c.i, iters,colormap[iters]);
+          Uint32 color =
+            colormap[iters];
+          putPixel(screen, screen_region.x + i, screen_region.y + j, color);
+        }
       //      putchar('\n');
     }
 
@@ -349,9 +369,9 @@ void draw_julia(SDL_Surface * screen,
     SDL_UnlockSurface(screen);
 
   // update!
-  /*  SDL_UpdateRect(screen, 
-		 screen_region.x, screen_region.y,
-		 screen_region.w, screen_region.h);*/
+  /*  SDL_UpdateRect(screen,
+      screen_region.x, screen_region.y,
+      screen_region.w, screen_region.h);*/
   //  SDL_UpdateRect(screen, 0,0,0,0);
   SDL_UpdateRects(screen, 1, &screen_region);
 
@@ -359,12 +379,7 @@ void draw_julia(SDL_Surface * screen,
 }
 
 unsigned julia_iterate(complex z, const complex c, double escape,
-		       unsigned maxiters)
+                       unsigned maxiters)
 {
-  unsigned iters = 0;
-  const double escsq = escape*escape;
-  while (++iters < maxiters && complex_sqmag(z) <= escsq)
-    z = complex_add(complex_mult(z, z), c);
-
-  return iters;
+  return iterate(z,c, maxiters);
 }
